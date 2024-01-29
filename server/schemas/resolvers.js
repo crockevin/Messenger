2541fa3727b7badd20cb56032eef03e916e811f6
Kevin <crochedengren@gmail.com>
2024-01-29 16:54:47 -0500

    started resolvers to view messages

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,12 @@
-const { User, Message, Conversation } = require('../')
+const { User, Message, Conversation } = require('../models')
 
 const resolvers = {
   Query: {
     users: async () => {
       return User.find().select('-password')
+    },
+    conversation: async (parent, { id }) => {
+      return Conversation.findById(id).populate('messages')
     },
   },
 }
