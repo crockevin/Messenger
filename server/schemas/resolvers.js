f75fe2b8dfbaece73c09a4a2ab25aefd0bae6af3
Kevin <crochedengren@gmail.com>
2024-02-01 02:58:51 -0500

    added code to add a message

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -15,12 +15,12 @@ const resolvers = {
       try {
         const user = await User.create({ username, email, password })
         if (!user) {
-          return console.log('ERROR')
+          throw new Error('ERROR')
         }
         const token = signToken(user)
         return { token, user }
       } catch (e) {
-        console.log(`ERROR: ${e}`)
+        throw new Error(e)
       }
     },
     login: async (parent, { email, password }) => {
@@ -36,7 +36,43 @@ const resolvers = {
         const token = signToken(user)
         return { token, user }
       } catch (e) {
-        console.log(`ERROR: ${e}`)
+        throw new Error(e)
+      }
+    },
+    addMessage: async (
+      parent,
+      { senderId, conversationId, content },
+      context
+    ) => {
+      try {
+        // if (!context.user) {
+        //   throw new Error('you are not logged in')
+        // }
+        // if (context.user._id !== senderId) {
+        //   throw new Error('You are not this user')
+        // }
+        const conversation = await Conversation.findById(conversationId)
+        if (!conversation) {
+          throw new Error('Invalid Conversation')
+        }
+        const inConversation = conversation.users.includes(senderId)
+        if (!inConversation) {
+          throw new Error('You are not in this conversation')
+        }
+        const message = await Message.create({
+          sender: senderId,
+          conversation: conversationId,
+          content,
+        })
+        if (!message) {
+          throw new Error('Message could not be created')
+        }
+        conversation.messages.push(message._id)
+        await conversation.save()
+        await message.populate('sender')
+        return message
+      } catch (e) {
+        throw new Error(e)
       }
     },
   },
