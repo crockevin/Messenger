e041a6c0c02d0f7008febbbb12b1a15ddced4e7c
Kevin <crochedengren@gmail.com>
2024-02-08 18:39:55 -0500

    added online status

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import '../src/index.css'
-import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink, split } from '@apollo/client'
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink, split, useMutation } from '@apollo/client'
 import { setContext } from '@apollo/client/link/context'
 import { QueryClientProvider, QueryClient } from 'react-query'
 import { WebSocketLink } from '@apollo/client/link/ws'
 import { getMainDefinition } from '@apollo/client/utilities'
 import Auth from "./utlis/auth";
+import { onlineStatus } from './utlis/mutation.js'
+
+
 
 const queryClient = new QueryClient()
 // Construct our main GraphQL API endpoint
@@ -52,6 +55,7 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 })
 const auth = Auth.loggedIn()
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
