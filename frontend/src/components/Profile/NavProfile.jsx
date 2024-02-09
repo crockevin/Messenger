acecd0f0d28bb7da56092675d5619b6925852038
Osmilliardo <140178301+Osmilliardo@users.noreply.github.com>
2024-02-09 13:40:40 -0500

    changed the layout on current user navProfile, added a button for send message on not current user profile

--- frontend/src/components/Profile/NavProfile.jsx
+++ frontend/src/components/Profile/NavProfile.jsx
@@ -35,54 +35,109 @@ export default function NavProfile() {
         minHeight: '80vh', // Adjusted minHeight to fit the content better
       }}
     >
-      <Paper
-        elevation={2}
-        sx={{
+      {!sameUser ? (
+        <Paper
+          elevation={3}
+          sx={{
             backgroundColor: '#013440',
-            color:'#e4ebf2',
+            color: '#e4ebf2',
             display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'space-around',
-          alignItems: 'center',
-          textAlign: 'center',
-          padding: 7.5,
-          width: '15%', // Adjusted width for better appearance
-          maxWidth: 400, // Added maxWidth to limit the width of Paper
-        }}
-      >
-        <Grid 
-        container 
-        direction="column" alignItems="center" spacing={2}>
-          <Grid item>
-            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-          </Grid>
-          <Grid item>
-            <Typography>{user.username}</Typography>
-          </Grid>
-          {/* send friend request */}
-          <Grid item>
-            {!sameUser ? (
+            flexDirection: 'column',
+            justifyContent: 'space-around',
+            alignItems: 'center',
+            textAlign: 'center',
+            padding: 7.5,
+            width: '15%', // Adjusted width for better appearance
+            maxWidth: 400, // Added maxWidth to limit the width of Paper
+          }}
+        >
+          <Grid container direction="column" alignItems="center" spacing={2}>
+            <Grid item>
+              <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+            </Grid>
+            <Grid item>
+              <Typography>{user.username}</Typography>
+            </Grid>
+            {/* send friend request */}
+            <Grid item>
               <Button
                 color="secondary"
                 variant="contained"
-                type="signup"
+                type="addFriend"
                 href="#"
               >
                 Add Friend
               </Button>
-            ) : (
+            </Grid>
+            <Grid item>
               <Button
+                color="secondary"
+                variant="contained"
+                type="newMessage"
+                href="#"
+              >
+                Send Message
+              </Button>
+            </Grid>
+          </Grid>
+        </Paper>
+      ) : (
+        <Grid container spacing={3}>
+          <Grid item xs={4}>
+            <Paper
+              elevation={3}
+              sx={{
+                backgroundColor: '#013440',
+                color: '#e4ebf2',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                padding: 7.5,
+              }}
+            >
+              <Grid item>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+              </Grid>
+              <Grid item>
+                <Typography>{user.username}</Typography>
+              </Grid>
+              <Button
+                color="secondary"
+                variant="contained"
+                type="editProfile"
+                href="/settings"
+              >
+                Edit Profile
+              </Button>
+            </Paper>
+          </Grid>
+          <Grid item xs={8}>
+            <Paper 
+              elevation={3}
+              sx={{
+                backgroundColor: '#e4ebf2',
+                color: '#013440',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                padding: 7.5,
+              }}>
+              <Typography>This is where the chats will go!</Typography>
+            </Paper>
+          </Grid>
+        </Grid>
+      )}
+    </Grid>
+  )
+}
+
+{
+  /* <Button
                 color="primary"
                 variant="contained"
                 type="signup"
                 href="/settings"
               >
                 Edit Profile
-              </Button>
-            )}
-          </Grid>
-        </Grid>
-      </Paper>
-    </Grid>
-  )
+              </Button> */
 }
