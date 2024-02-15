67196c6b9262f10700fcb54161ea5a6b0d7a14fe
nic-d <nicanthonidiaz@gmail.com>
2024-02-15 18:07:00 -0500

    added deployed app link to readme file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,6 @@
 // Database connection
 const mongoose = require('mongoose')
-require('dotenv').config()
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/ram-ram-ranch')
+mongoose.connect('mongodb://127.0.0.1:27017/ram-ram-ranch')
 
 module.exports = mongoose.connection
